9c16abf7afeff7a753b607c3388bc432442e9aa7
iqeq00 <iqeq00@gmail.com>
2020-03-29 18:57:51 +0800

    first commit

---
+++ scripts/home.js
@@ -0,0 +1,34 @@
+function fontLength(title, num) {
+    var val = title.replace(/\s+/g, "");
+    if (val.length > num) {
+        val = val.substr(0, num) + "...";
+    }
+    return val;
+}
+
+$(".fullSlide").slide({
+    titCell: ".hd ul",
+    mainCell: ".bd ul",
+    effect: "leftLoop",
+    vis: "auto",
+    autoPlay: true,
+    autoPage: true,
+    trigger: "click"
+});
+
+$(".noticeList a").each(function (index, element) {
+    var tit = fontLength($(this).html(), 25)
+    $(this).html(tit)
+});
+
+$(".noticeList li").hover(function () {
+    $(this).addClass("active");
+}, function () {
+    $(this).removeClass("active");
+});
+
+$(".resources li").hover(function () {
+    $(this).addClass("active");
+}, function () {
+    $(this).removeClass("active");
+});
