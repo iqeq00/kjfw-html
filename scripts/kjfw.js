e076659cd7b5ef85c2370c636db5297f5a713a34
iqeq00 <iqeq00@gmail.com>
2020-03-31 16:53:06 +0800

    Update kjfw.html

--- scripts/kjfw.js
+++ scripts/kjfw.js
@@ -8,4 +8,10 @@ $(function () {
         autoPage: true,
         trigger: "click"
     });
+
+    $(".information li").hover(function () {
+        $(this).addClass("active");
+    }, function () {
+        $(this).removeClass("active");
+    });
 });
