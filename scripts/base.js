eb930699b2cdefa636f2947db62a3ec58b04b1d7
iqeq00 <iqeq00@gmail.com>
2020-04-01 18:31:05 +0800

    Add header mark title

--- scripts/base.js
+++ scripts/base.js
@@ -1,4 +1,20 @@
+function getShorturl() {
+    var url = window.location.href;
+    var index = url.lastIndexOf("\/");
+    return url.substring(index,url.length);
+}
+
 $(function () {
-    $(".header").load("../html/header.html");
+    var url = getShorturl();
+    $(".header").load("../html/header.html", function () {
+        $(".menu li .menuA").each(function () {
+            $(this).removeClass("active");
+            var lang = $(this).attr("lang");
+            if (url.indexOf(lang) > 0) {
+                $(this).addClass("active");
+            }
+        });
+    });
+
     $(".footer").load("../html/footer.html");
 });
